6d71b1c5e5bd578465377bae6b4448f5d638c8f1
Anshika Singh <anshikasingh11720@gmail.com>
2026-06-09 21:53:03 +0530

    updated files

--- Day9/q2.cpp
+++ Day9/q2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int n=5;
     for(int i=5;i>=1;i--){
-        for(int j=1;j<=i;j++){
-            cout<<"*";
+        for (int j=1 ;j<=i;j++){
+            cout<<j;
         }
         cout<<endl;
     }
--- Day9/q3.cpp
+++ Day9/q3.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int n=5;
-    for(int i=5;i>=1;i--){
+    char ch ='A';
+    for(int i=1;i<=5;i++){
         for(int j=1;j<=i;j++){
-            cout<<"*";
+            cout<<ch;
         }
         cout<<endl;
+        ch++;
     }
     return 0;
 }
