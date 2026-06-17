e78488bd8c0931a67218bcce8fd2b75ea44d1aaf
Anshika Singh <anshikasingh11720@gmail.com>
2026-06-17 10:39:18 +0530

    updated files

--- Dayy14/q1.cpp
+++ Dayy14/q1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n,i,position=-1,key;
+
+    cout<<"enter the number of elements";
+    cin>>n;
+
+    int a[n];
+    cout<<"enter the elements";
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"enter element you have to search";
+    cin>>key;
+
+    for(i=0;i<n;i++){
+        if(a[i]==key){
+            position=i;
+            break;
+        }
+    }
+    if(position==-1){
+        cout<<"element not found";
+    }
+    else{
+        cout<<"element is at position"<<position+1;
+    }
+    return 0;
+}
---
+++ Dayy14/q3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int a[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 1;
+
+        if (a[i] == -1)
+            continue;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] == a[j])
+            {
+                count++;
+                a[j] = -1;   // Mark as counted
+            }
+        }
+
+        cout << a[i] << " occurs " << count << " times" << endl;
+    }
+
+    return 0;
+}
